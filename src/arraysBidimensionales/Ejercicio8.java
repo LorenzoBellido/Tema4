3f4ccedcd205b6ac6774e129564ba78a411df5b0
loren <loren@Lorenzo>
2024-02-04 13:36:01 +0100

    funcion

--- src/arraysBidimensionales/Ejercicio8.java
+++ src/arraysBidimensionales/Ejercicio8.java
@@ -5,29 +5,37 @@ public class Ejercicio8 {
 	public static void main(String[] args) {
 
 		// Creo una tabla bidimensional
-		int tabla[][] = {{5,2},{2,1}};
-		
-		if(simetrica(tabla)) {
+		int tabla[][] = { { 1, 5 }, { 2, 1 } };
+
+		// Condicion según el booleano que devuelve la funcion simetrica
+		if (simetrica(tabla)) {
 			System.out.println("Es simétrica");
-		}else {
+		} else {
 			System.out.println("No es simétrica");
 		}
 	}
 
+	/**
+	 * Funcion que devuelve si el Array bidimensional es simétrico o no recorriendo
+	 * una tabla introducida por paarametros
+	 * 
+	 * @param t Tabla bidimensional
+	 * @return
+	 */
 	public static boolean simetrica(int t[][]) {
 		boolean simetrica = true;
-		
+
 		int tabla[][] = t;
-		
-		for(int i = 0; i < tabla.length; i++) {
-			for(int j = 0; j < tabla[i].length; j++) {
-				if(tabla[i][j] != tabla[j][i]) {
+
+		for (int i = 0; i < tabla.length; i++) {
+			for (int j = 0; j < tabla[i].length; j++) {
+				if (tabla[i][j] != tabla[j][i]) {
 					simetrica = false;
 					break;
 				}
-			
+
 			}
-		}		
+		}
 		return simetrica;
 	}
 }
---
+++ src/arraysBidimensionales/Ejercicio9.java
@@ -0,0 +1,23 @@
+package arraysBidimensionales;
+
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+		
+		int tabla[][] = {{2,2},{4,3}};
+		
+		if(esMagica(tabla)) {
+			System.out.println("La tabla es mágica");
+		}else {
+			System.out.println("La tabla no es mágica");
+		}
+
+	}
+
+	public static boolean esMagica(int tabla[][]) {
+		boolean esMagica = false;
+
+	
+		return esMagica;
+	}
+}
